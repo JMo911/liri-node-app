6a480abeb59b54f572b4442969973533fac237e3
Jacob Moss <jacobmoss09@gmail.com>
2019-07-30 21:26:09 -0500

    bandsintown api call works and returns all desired info

--- liri.js
+++ liri.js
@@ -2,24 +2,34 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
-console.log(spotify);
+// console.log(spotify);
 
 var axios = require('axios');
 
+var command = process.argv[2];
+if (command==="concert-this") {
+    concertThis(process.argv[3]);
+};
+
 function concertThis(artist) {
-    // artist = process.argv[2];
 
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=6e448e5f65047313d906d08b6ec4c016")
     .then(function (response) {
         // handle success
-        console.log(response);
+        var data = response.data;
+        data.forEach(function(e){
+            console.log("Venue: " + e.venue.name);
+            console.log("City: " + e.venue.city + " Country: " + e.venue.country);
+            console.log(moment(e.datetime).format('L') + "\n");
+        })
     })
     .catch(function (error) {
         // handle error
         console.log(error);
     });
 };
-concertThis(process.argv[2]);
+
 
